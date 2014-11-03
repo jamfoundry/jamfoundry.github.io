a631f5259766cb08b23a8e90a0d2c756353a039e
Jase Wells <jase@jasewells.com>
2014-11-02 16:54:43 -0800

    Added real logo.

--- js/jam.js
+++ js/jam.js
@@ -1,7 +1,6 @@
 function splashsize () {
 	var n = $("nav").height();
 	var h = $(window).height() - $(".splash").height();
-	console.log("height", h);
 	$(".splash").css("paddingTop", (h / 2 + n) + "px");
 	$(".splash").css("paddingBottom", (h / 2) + "px");
 }
@@ -18,5 +17,14 @@ $(document).ready(function(){
 	$(".make-it-appear-right").waypoint(function(){
 		$(this).addClass("animated fadeInRight");
 	}, {offset: "80%"});
+	
+	$(window).on("scroll", function(e){
+		var top = $(document).scrollTop();
+		if (top > 20) {
+			$("nav").addClass("navbar-scrolled");
+		} else {
+			$("nav").removeClass("navbar-scrolled");
+		}
+	});
 	
 });
